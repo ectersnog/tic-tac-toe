7a48ce78801a3b0f83de24d0f56e035b2a609b5d
Jason <jdamundsen@gmail.com>
2025-09-11 17:09:00 -0500

    board class updates

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -5,8 +5,7 @@ module TicTacToe
     attr_reader :board, :board_view
 
     def initialize(board = nil)
-      @board = board || Array.new(9, '-')
-      @board_view = self.class.generate_board_view(@board)
+      self.board = board || Array.new(9, '-')
     end
 
     def board=(input)
@@ -14,12 +13,18 @@ module TicTacToe
       @board_view = self.class.generate_board_view(@board)
     end
 
+    def play_square(position, player)
+      new_board = @board.dup
+      new_board[position] = player
+      self.board = new_board
+    end
+
+    def square_free?(position)
+      @board[position] == "-"
+    end
+
     def self.generate_board_view(board)
-      board_view = []
-      board.each_slice(3) do |row|
-        board_view << row
-      end
-      board_view
+      board.each_slice(3).to_a
     end
   end
 end
